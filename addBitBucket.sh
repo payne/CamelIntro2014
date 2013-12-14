5e62d70aed847edf942acb87426d8779c9e8baee
Matt Payne <Payne@MattPayne.org>
2013-12-13 19:55:28 -0600

    Save

---
+++ addBitBucket.sh
@@ -0,0 +1,3 @@
+git remote add originBB ssh://git@bitbucket.org/MattPayneOrg/camelintro2014private.git
+git push -u originBB --all # pushes up the repo and its refs for the first time
+git push -u originBB --tags # pushes up any tags
